777554db28b497ab8519a3db892aa5101ba46253
Carolina Batatinha <batatinha.carolina@gmail.com>
2023-07-20 09:53:18 -0300

    Add files via upload

---
+++ ex001/Program.cs
@@ -0,0 +1,17 @@
+﻿//1.	Faça um algoritmo para imprimir 10 vezes o nome da disciplina (uma em cada linha).
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Digite o nome de uma disciplina: ");
+        string disciplina = Console.ReadLine();
+
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine(disciplina);
+        }
+    }
+}
