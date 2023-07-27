b35a38ee6b7167972c7fff54e13af0758fd35abc
Carolina Batatinha <batatinha.carolina@gmail.com>
2023-07-27 10:04:00 -0300

    Update Program.cs
    Melhoria no código

--- ex035/Program.cs
+++ ex035/Program.cs
@@ -1,4 +1,4 @@
-﻿// 35.	Escrever um algoritmo que um número indefinido de valores, para cada entrada de dados a entrada de 2 valores, o primeiro representando o número de um aluno, e o segundo representando a sua altura em centímetros. Encontre o aluno mais alto e o mais baixo. Mostre o número do aluno mais alto e do mais baixo, junto com suas alturas. 
+// 35.	Escrever um algoritmo que um número indefinido de valores, para cada entrada de dados a entrada de 2 valores, o primeiro representando o número de um aluno, e o segundo representando a sua altura em centímetros. Encontre o aluno mais alto e o mais baixo. Mostre o número do aluno mais alto e do mais baixo, junto com suas alturas. 
 
 using System;
 
@@ -14,13 +14,13 @@ class Program
         while (true)
         {
             Console.Write("Digite o número do aluno (ou 0 para encerrar): ");
-            int numeroAluno = int.Parse(Console.ReadLine());
+            int numeroAluno = Convert.ToInt32(Console.ReadLine());
 
             if (numeroAluno == 0)
                 break;
 
             Console.Write("Digite a altura do aluno em centímetros: ");
-            int altura = int.Parse(Console.ReadLine());
+            int altura = Convert.ToInt32(Console.ReadLine());
 
             if (altura > alturaMaisAlta)
             {
@@ -35,12 +35,12 @@ class Program
             }
         }
 
-        Console.WriteLine("Aluno mais baixo:");
-        Console.WriteLine("Número do aluno: " + alunoMaisBaixo);
-        Console.WriteLine("Altura: " + alturaMaisBaixa + " cm");
+        Console.WriteLine("\n## Aluno mais baixo ##");
+        Console.WriteLine($"Número do aluno: {alunoMaisBaixo}");
+        Console.WriteLine($"Altura: {alturaMaisBaixa}cm");
         Console.WriteLine("==============================");
-        Console.WriteLine("Aluno mais alto:");
-        Console.WriteLine("Número do aluno: " + alunoMaisAlto);
-        Console.WriteLine("Altura: " + alturaMaisAlta + " cm");
+        Console.WriteLine("\n## Aluno mais alto ##");
+        Console.WriteLine($"Número do aluno: {alunoMaisAlto}" );
+        Console.WriteLine($"Altura: {alturaMaisAlta}cm");
     }
 }
