c7c0685059f854b33d9ae363860b47ad9fdd4737
Carolina Batatinha <batatinha.carolina@gmail.com>
2023-08-28 15:51:42 -0300

    Melhoria no código

--- ex006/Program.cs
+++ ex006/Program.cs
@@ -6,12 +6,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Digite um número:");
+        Console.WriteLine("Digite um número: ");
         int num = Convert.ToInt32(Console.ReadLine());
 
         int soma = 0;
 
-        Console.WriteLine($"Os números de 1 até {num} são :");
+        Console.WriteLine($"Os números de 1 até {num} são: ");
         for (int i = 1; i <= num; i++)
         {
             Console.WriteLine(i);
