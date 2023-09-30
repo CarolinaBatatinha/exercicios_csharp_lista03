33a34f73af7f7fb02e710c298f7d650e29fad1ef
Carolina Batatinha <batatinha.carolina@gmail.com>
2023-09-30 13:16:32 -0300

    Correção no código

--- ex006/Program.cs
+++ ex006/Program.cs
@@ -1,4 +1,4 @@
-﻿// 6.	Elabore um algoritmo para calcular a soma dos números impares de 0 a 100.
+// 6.	Elabore um algoritmo para calcular a soma dos números impares de 0 a 100.
 
 using System;
 
@@ -6,18 +6,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Digite um número: ");
+        Console.WriteLine("Digite um número:");
         int num = Convert.ToInt32(Console.ReadLine());
 
         int soma = 0;
 
-        Console.WriteLine($"Os números de 1 até {num} são: ");
+        Console.WriteLine($"Os números de 1 até {num} são :");
         for (int i = 1; i <= num; i++)
         {
-            Console.WriteLine(i);
-            soma += i;
+            if (i % 2 == 1)
+            {
+                Console.Write(i + " ");
+                soma += i;
+            }
         }
 
-        Console.WriteLine($"A soma desses números é igual a {soma}.");
+        Console.WriteLine($"\nA soma desses números é igual a {soma}.");
     }
 }
